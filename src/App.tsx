f7263198ddd6d284a9314d4f117d9c8ac64d7f85
adamskikacper <adamskikacper@users.noreply.github.com>
2025-06-15 22:27:51 +0100

    feat: barrel imports for pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { TooltipProvider } from "@/components/UI/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
-import Home from "./pages/Home";
-import JobList from "./pages/Jobs/JobList";
-import JobDetails from "./pages/Jobs/JobDetails";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound/NotFound";
-import DashboardLayout from "./pages/Dashboard/DashboardLayout";
+import {
+ Home,
+ JobList,
+ JobDetails,
+ Login,
+ NotFound,
+ DashboardLayout,
+} from "./pages";
 import { DashboardRoutes } from "./components/Dashboard";
 
 const queryClient = new QueryClient();
