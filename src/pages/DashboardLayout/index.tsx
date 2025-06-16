ba114ea81685d37120613b5ec769f60030fbe8af
adamskikacper <adamskikacper@users.noreply.github.com>
2025-06-16 22:48:35 +0100

    feat: temporary fix

--- src/pages/DashboardLayout/index.tsx
+++ src/pages/DashboardLayout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks/useAuth";
 import { DashboardLayout as LayoutComponent } from "@/components/Dashboard/Layout";
-import { LoadingScreen } from "@/components/Common";
+import LoadingScreen from "@/components/Common/LoadingScreen";
 
 /**
  * DashboardLayout - Layout wrapper for all dashboard pages
