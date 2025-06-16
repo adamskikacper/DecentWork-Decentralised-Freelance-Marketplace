eb0f9ab6272f15fc3a8dd54b5c2d0bed80ffce73
Kacper Adamski <kacperadamski@Kacpers-MacBook-Pro.local>
2025-06-16 23:12:03 +0100

    feat: temporary fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-export * from "./DashboardLayout";
-export * from "./Home";
-export * from "./Login";
-export * from "./NotFound";
-export * from "./Jobs/JobList";
-export * from "./Jobs/JobDetails";
-export * from "./DashboardLayout";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as NotFound } from "./NotFound";
+export { default as DashboardLayout } from "./DashboardLayout";
+
+export { default as JobList } from "./Jobs/JobList";
+export { default as JobDetails } from "./Jobs/JobDetails";
+
+export * as Jobs from "./Jobs";
