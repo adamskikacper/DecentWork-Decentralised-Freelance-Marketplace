392c596b2cea5579fdcb4f5bc6a0f83e17c6ecc4
adamskikacper <adamskikacper@users.noreply.github.com>
2025-06-16 22:37:46 +0100

    feat: temporary fix

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
-import { useAuth } from "../../hooks/useAuth";
-import LoginForm from "../../components/Auth/LoginForm";
+import { useAuth } from "@/hooks/useAuth";
+import LoginForm from "@/components/Auth/LoginForm";
 import SignupForm from "@/components/Auth/SignupForm";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/UI";
