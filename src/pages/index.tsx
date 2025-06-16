7e746b95d40636c43cee04d70d103b341c1c59c8
adamskikacper <adamskikacper@users.noreply.github.com>
2025-06-16 22:08:13 +0100

    feat: imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 export { default as Home } from "./Home";
 export { default as Login } from "./Login";
-export { default as NotFound } from "./NotFound/NotFound";
+export { default as NotFound } from "./NotFound";
 export { default as JobList } from "./Jobs/JobList";
 export { default as JobDetails } from "./Jobs/JobDetails";
-export { default as DashboardLayout } from "./Dashboard/DashboardLayout";
+export { default as DashboardLayout } from "./DashboardLayout";
