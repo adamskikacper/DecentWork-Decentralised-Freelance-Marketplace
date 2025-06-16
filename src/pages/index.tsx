3c62adbd08ced6448915f36b46b53c7289400d6c
Kacper Adamski <kacperadamski@Kacpers-MacBook-Pro.local>
2025-06-16 22:23:11 +0100

    feat: index fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-export { default as Home } from "./Home/index.tsx";
-export { default as Login } from "./Login/index.tsx";
-export { default as NotFound } from "./NotFound/index.tsx";
-export { default as JobList } from "./Jobs/JobList/index.tsx";
-export { default as JobDetails } from "./Jobs/JobDetails/index.tsx";
-export { default as DashboardLayout } from "./DashboardLayout/index.tsx";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as NotFound } from "./NotFound";
+export { default as JobList } from "./Jobs/JobList";
+export { default as JobDetails } from "./Jobs/JobDetails";
+export { default as DashboardLayout } from "./DashboardLayout";
