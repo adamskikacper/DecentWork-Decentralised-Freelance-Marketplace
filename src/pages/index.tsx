d2bd4da8af7c6ad2097ca701e2b7fc7d0648402c
adamskikacper <adamskikacper@users.noreply.github.com>
2025-06-18 22:58:48 +0100

    feat: shared barrel imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-export { default as Home } from "./Home/index.tsx";
-export { default as Login } from "./Login/index.tsx";
-export { default as NotFound } from "./NotFound/index.tsx";
-export { default as DashboardLayout } from "./DashboardLayout/index.tsx";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as NotFound } from "./NotFound";
+export { default as DashboardLayout } from "./DashboardLayout";
 
-export { default as JobList } from "./Jobs/JobList/index.tsx";
-export { default as JobDetails } from "./Jobs/JobDetails/index.tsx";
+export { default as JobList } from "./Jobs/JobList";
+export { default as JobDetails } from "./Jobs/JobDetails";
 
-export * as Jobs from "./Jobs/index.tsx";
+export * as Jobs from "./Jobs";
