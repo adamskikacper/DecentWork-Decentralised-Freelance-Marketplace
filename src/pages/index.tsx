c4f97e959c8602da001d9a36c90516d8a47f93b7
Kacper Adamski <kacperadamski@Kacpers-MacBook-Pro.local>
2025-06-18 20:34:42 +0100

    feat: barrel imports fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-export { default as Home } from "./Home/index.tsx";
-export { default as Login } from "./Login/index.tsx";
-export { default as NotFound } from "./NotFound/index.tsx";
-export { default as DashboardLayout } from "./DashboardLayout/index.tsx";
+export { default as Home } from "./Home/index";
+export { default as Login } from "./Login/index";
+export { default as NotFound } from "./NotFound/index";
+export { default as DashboardLayout } from "./DashboardLayout/index";
 
-export { default as JobList } from "./Jobs/JobList/index.tsx";
-export { default as JobDetails } from "./Jobs/JobDetails/index.tsx";
+export { default as JobList } from "./Jobs/JobList/index";
+export { default as JobDetails } from "./Jobs/JobDetails/index";
 
-export * as Jobs from "./Jobs/index.tsx";
+export * as Jobs from "./Jobs/index";
