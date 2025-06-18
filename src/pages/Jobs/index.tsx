decd8f88962df6123c7b94498a41f75ebd9e4ea8
Kacper Adamski <kacperadamski@Kacpers-MacBook-Pro.local>
2025-06-18 22:58:48 +0100

    feat: shared barrel imports

--- src/pages/Jobs/index.tsx
+++ src/pages/Jobs/index.tsx
@@ -1,2 +1,2 @@
-export { default as JobList } from "./JobList/index.tsx";
-export { default as JobDetails } from "./JobDetails/index.tsx";
+export { default as JobList } from "./JobList";
+export { default as JobDetails } from "./JobDetails";
