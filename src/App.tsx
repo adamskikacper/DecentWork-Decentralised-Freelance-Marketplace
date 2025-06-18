f8b5cd232a4224f67c6470ea55a4228ad0b0e7c4
adamskikacper <adamskikacper@users.noreply.github.com>
2025-06-18 20:19:57 +0100

    feat: barrel imports fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import DashboardLayout from "./pages/DashboardLayout";
-import JobList from "./pages/Jobs/JobList";
-import JobDetails from "./pages/Jobs/JobDetails";
+import Home from "./pages/Home/index";
+import Login from "./pages/Login/index";
+import NotFound from "./pages/NotFound/index";
+import DashboardLayout from "./pages/DashboardLayout/index";
+import JobList from "./pages/Jobs/JobList/index";
+import JobDetails from "./pages/Jobs/JobDetails/index";
 import { DashboardRoutes } from "./components/Dashboard";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { Toaster } from "sonner";
