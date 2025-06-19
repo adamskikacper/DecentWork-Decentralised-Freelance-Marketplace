e71a8737489380d3c5ece7f883863c596b795fa7
Kacper Adamski <kacperadamski@Kacpers-MacBook-Pro.local>
2025-06-19 20:22:47 +0100

    feat: standardized exports

--- src/components/Profile/index.ts
+++ src/components/Profile/index.ts
@@ -1,6 +1,6 @@
 export { ClientInfo } from "./ClientInfo";
 export { FreelancerDetails } from "./FreelancerDetails";
 export { FreelancerTable } from "./FreelancerTable";
-export { FreelancerCard } from "./ProfileCard/FreelancerCard";
+export { FreelancerCard } from "./FreelancerCard";
 export { TestimonialCard } from "./Reviews/TestimonialCard";
 export { SkillSelect } from "./SkillsList/SkillSelect";
