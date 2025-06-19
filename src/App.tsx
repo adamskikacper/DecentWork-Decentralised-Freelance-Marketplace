48c0eddebc372697144057dd8b4b73f430d02380
Kacper Adamski <kacperadamski@Kacpers-MacBook-Pro.local>
2025-06-19 19:27:45 +0100

    bugfix: UI components not having correct case sensitivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Toaster } from "@/components/ui/Toaster";
-import { Toaster as Sonner } from "@/components/ui/Sonner";
-import { TooltipProvider } from "@/components/ui/Tooltip";
+import { Toaster } from "@/components/UI/Toaster";
+import { Toaster as Sonner } from "@/components/UI/Sonner";
+import { TooltipProvider } from "@/components/UI/Tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
